bfbd179050f47d49c100e600d3433491e23da4dc
JakkeLab <itsjakk3@gmail.com>
2023-10-29 22:30:41 +0900

    Part 1-3. Code Structure and Types

---
+++ src/Part1-1_CSharpBasic/01_코드구조.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Part1_3_1_WriteCode
+{
+    //클래스
+    public class WriteCode
+    {
+        public string MyName;
+        //메소드(함수)
+        static void Main(string[] args)
+        {
+            //메소드 내에서 실행하는 내용
+            Greeting greeting = new Greeting();
+            greeting.Name = "Jakke";
+            greeting.SayHello();
+
+            //종료 방지용.
+            Console.ReadLine();
+        }
+    }
+    
+    //클래스
+    public class Greeting
+    {
+        //필드
+        public string Name;
+
+        //메소드(함수)
+        public void SayHello()
+        {
+            Console.WriteLine("Hello, " + this.Name);
+        }
+    }
+}
+
+
+
+
+
---
+++ src/Part1-3_2_Types/02_타입.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1_3_2_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("예제1번");
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(a + b);           //30 출력
+
+            Console.WriteLine("\n예제2번");
+            double d1 = 10.23;
+            double d2 = 5.01;
+            Console.WriteLine(d1 + d2);         //15.24 출력
+
+            Console.WriteLine("\n예제3번");
+            string str1 = "Revit";
+            string str2 = "Programming";
+            Console.WriteLine(str1 + str2);     //RevitProgramming 출력
+
+            Console.WriteLine("\n예제4번");
+            char c1 = 'R';
+            char c2 = 'V';
+            char c3 = 'T';
+            Console.WriteLine(c1 + c2 + c3);    //252 출력
+            Console.WriteLine(c1.ToString() + c2 + c3);    //RVT 출력
+
+            //종료 방지용.
+            Console.ReadLine();
+        }
+    }
+}
