01e8b7daba13f9ceb580ca185279cea50f47cfc9
JakkeLab <itsjakk3@gmail.com>
2023-12-13 22:34:53 +0900

    OOP - Inheritance And Readonly

---
+++ src/Part1-7_3_OOP_Inheritance/Furnitures.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace OOP.Basic.Furnitures
+{
+    //책상 클래스
+    public class Desk
+    {
+        #region 책상의 속성
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public double Height { get; set; }
+        public virtual TableTops TableTop { get; }
+        public virtual int Price { get;  }
+        #endregion
+
+        #region 책상 유형 기본값
+        public enum TableTops
+        {
+            NotDefined,
+            Maple,
+            Mahogany,
+            Ash
+        }
+        #endregion
+
+        #region 책상관련 메소드
+
+        //상판 종류 보여주기
+        public static string ShowTableTops()
+        {
+            string result = string.Empty;
+            int cnt = 1;
+            foreach(TableTops tb in Enum.GetValues(typeof(TableTops)))
+            {
+                if(tb != TableTops.NotDefined)
+                {
+                    result += $"{cnt}. {tb}\n";
+                    cnt++;
+                }
+            }
+            return result;
+        }
+
+        //책상의 크기 정보 받기
+        public string GetSize()
+        {
+            return $"W : {this.Width}, D : {this.Length}, H : {this.Height}";
+        }
+
+        //책상의 상판 정보 받기
+        public TableTops GetTableTop()
+        {
+            return this.TableTop;
+        }
+
+        //책상의 가격정보 불러오기
+        public int GetPrice()
+        {
+            return this.Price;
+        }
+        #endregion
+
+        #region 책상 생성자
+
+        //기본 책상 생성
+        public Desk()
+        {
+            this.Width = 1200;
+            this.Length = 600;
+            this.Height = 750;
+            this.TableTop = TableTops.NotDefined;
+            this.Price = 80000;
+        }
+
+        //규격과 가격을 직접 입력해서 생성
+        public Desk(double width, double length, double height, int price)
+        {
+            this.Width = width;
+            this.Length = length;
+            this.Height = height;
+            this.TableTop = TableTops.NotDefined;
+            this.Price = price;
+        }
+
+        //상판만 직접 지정해서 생성
+        public Desk(string tableTop, int price)
+        {
+            this.Width = 1200;
+            this.Length = 600;
+            this.Height = 750;
+            this.TableTop = TableTops.NotDefined;
+            this.Price = price;
+        }
+
+        //전부 옵션을 지정해서 생성하기
+        public Desk(double width, double length, double height, TableTops tableTop, int price)
+        {
+            this.Width = width;
+            this.Length = length;
+            this.Height = height;
+            this.TableTop = tableTop;
+            this.Price = price;
+        }
+        #endregion
+    }
+
+    //Maple 책상 정의
+    public class MapleDesk : Desk
+    {
+        public MapleDesk(double width, double length, double height)
+        {
+            this.Width = width;
+            this.Length = length;
+            this.Height = height;
+        }
+
+        //기본사이즈 책상 생성자
+        public MapleDesk()
+        {
+
+        }
+
+        public override int Price { get; } = 120000;
+        public override TableTops TableTop { get; } = TableTops.Maple; 
+    }
+
+    //Mahogany 책상 정의
+    public class MahoganyDesk : Desk
+    {
+        public MahoganyDesk(double width, double length, double height)
+        {
+            this.Width = width;
+            this.Length = length;
+            this.Height = height;
+        }
+
+        //기본사이즈 책상 생성자
+        public MahoganyDesk()
+        {
+
+        }
+
+        public override int Price { get; } = 180000;
+        public override TableTops TableTop { get; } = TableTops.Mahogany;
+    }
+
+    //Ash 책상 정의
+    public class AshDesk : Desk
+    {
+        public AshDesk(double width, double length, double height)
+        {
+            this.Width = width;
+            this.Length = length;
+            this.Height = height;
+        }
+
+        //기본사이즈 책상 생성자
+        public AshDesk()
+        {
+
+        }
+
+        public override int Price { get; } = 150000;
+        public override TableTops TableTop { get; } = TableTops.Ash;
+    }
+}
---
+++ src/Part1-7_3_OOP_Inheritance/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using OOP.Basic.Furnitures;
+using static OOP.Basic.Furnitures.Desk;
+
+namespace _01_Basic
+{
+    public class GetOrder
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("책상 주문하기");
+            Console.WriteLine("==========");
+            Console.WriteLine("주문 방법 선택하기");
+            Console.WriteLine("1. 기본형 책상 구매하기(1200*600*750, 메이플 옵션");
+            Console.WriteLine("2. 크기만 정해서 구입하기(메이플 상판)");
+            Console.WriteLine("3. 1200*600 책상의 상판만 바꿔서 구매하기");
+            Console.WriteLine("4. 전부 커스텀 하기");
+            Console.WriteLine("5. 상판 종류 보기");
+            Console.WriteLine("6. 책상 종류별 기본사이즈 가격 보기");
+            Console.WriteLine("==========");
+            while (true)
+            {
+                Console.Write("구매 방법을 선택하세요 : ");
+                int order = int.Parse(Console.ReadLine());
+                Console.WriteLine("\n");
+                Desk desk;
+                switch (order)
+                {
+                    case 1:
+                        //기본형 책상 생성
+                        desk = new Desk();
+                        Console.WriteLine("=========");
+                        Console.WriteLine("구매내역");
+                        Console.WriteLine($"규격 : {desk.GetSize()}");
+                        Console.WriteLine($"상판 : {desk.GetTableTop()}");
+                        Console.WriteLine($"가격 : {desk.GetPrice()}");
+                        Console.WriteLine("=========");
+                        break;
+
+                    case 2:
+                        //커스텀 크기 설정
+                        desk = new Desk("Mahogany", 150000);
+                        Console.WriteLine("=========");
+                        Console.WriteLine("구매내역");
+                        Console.WriteLine($"규격 : {desk.GetSize()}");
+                        Console.WriteLine($"상판 : {desk.GetTableTop()}");
+                        Console.WriteLine($"가격 : {desk.GetPrice()}");
+                        Console.WriteLine("=========");
+                        break;
+                    case 3:
+                        //상판만 변경
+                        desk = new Desk(1200, 600, 750, 100000);
+                        Console.WriteLine("=========");
+                        Console.WriteLine("구매내역");
+                        Console.WriteLine($"규격 : {desk.GetSize()}");
+                        Console.WriteLine($"상판 : {desk.GetTableTop()}");
+                        Console.WriteLine($"가격 : {desk.GetPrice()}");
+                        Console.WriteLine("=========");
+                        break;
+                    case 4:
+                        //전부 커스텀
+                        desk = new Desk(1800, 700, 750, TableTops.NotDefined, 220000);
+                        Console.WriteLine("=========");
+                        Console.WriteLine("구매내역");
+                        Console.WriteLine($"규격 : {desk.GetSize()}");
+                        Console.WriteLine($"상판 : {desk.GetTableTop()}");
+                        Console.WriteLine($"가격 : {desk.GetPrice()}");
+                        Console.WriteLine("=========");
+                        break;
+                    case 5:
+                        Console.WriteLine(Desk.ShowTableTops());
+                        break;
+                    case 6:
+                        Console.WriteLine(GetAllDeskPrices());
+                        break;
+                    default:
+                        Console.WriteLine("잘못 입력하였습니다. 다시 입력해 주세요.");
+                        break;
+                }
+                Console.WriteLine("주문내역을 더 확인하겠습니까?(Y/N)");
+                string checkNewOrder = Console.ReadLine();
+                if (checkNewOrder == "Y")
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("주문확인을 끝냅니다.");
+                }
+            }
+        }
+
+        public int GetPrice(Desk desk)
+        {
+            return desk.GetPrice();
+        }
+
+        public static string GetAllDeskPrices()
+        {
+            string result = string.Empty;
+            var ashDesk = new AshDesk();
+            var mapleDesk = new MapleDesk();
+            var mahoganyDesk = new MahoganyDesk();
+            result += $"{ashDesk.TableTop} 책상({ashDesk.GetSize()}) : {ashDesk.GetPrice()}\n";
+            result += $"{mapleDesk.TableTop} 책상({mapleDesk.GetSize()}) : {mapleDesk.GetPrice()}\n";
+            result += $"{mahoganyDesk.TableTop} 책상({mahoganyDesk.GetSize()}) : {mahoganyDesk.GetPrice()}\n";
+
+            return result;
+        }
+    }
+}
